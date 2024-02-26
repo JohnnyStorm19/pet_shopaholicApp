956542fa2172778adbfe9f887fe32433456346ad
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-02-26 12:17:42 +0300

    refactor: refactor Banner component by DRY

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,56 +1,47 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
-import 'swiper/css';
-import '../styles/swiperBanner.css'
-import { useNavigate } from 'react-router-dom';
+import "swiper/css";
+import "../styles/swiperBanner.css";
+import { useNavigate } from "react-router-dom";
+
+const images = [
+  "/images/1.jpg",
+  "/images/2.jpg",
+  "/images/4.jpg",
+  "/images/5.jpg",
+  "/images/6.jpg",
+  "/images/7.jpg",
+];
 
 const Banner = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleBannerClick = (image: string) => {
+    if (image === '/images/4.jpg' || image === '/images/5.jpg') {
+        console.log('Banner clicked')
+        navigate('/catalogue');
+    }
+  }
 
   return (
-        <Swiper 
-            slidesPerView={1}
-            modules={[Autoplay]}
-            autoplay={{
-                delay: 3500,
-                disableOnInteraction: false,
-            }}
-        >
-            <SwiperSlide
-                onClick={() => console.log('clicked frame 1!')}
-            >
-                <img src="/images/1.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide
-                onClick={() => console.log('clicked frame 2!')}
-            >
-                <img src="/images/2.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide
-                onClick={() => navigate('/catalogue')}
-            >
-                <img src="/images/4.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide
-                onClick={() => navigate('/catalogue')}
-            >
-                <img src="/images/5.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide
-                onClick={() => console.log('clicked frame 5!')}
-            >
-                <img src="/images/6.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide
-                onClick={() => console.log('clicked frame 6!')}
-            >
-                <img src="/images/7.jpg" alt="" />
-            </SwiperSlide>
-        </Swiper>
-
+    <Swiper
+      slidesPerView={1}
+      modules={[Autoplay]}
+      autoplay={{
+        delay: 3500,
+        disableOnInteraction: false,
+      }}
+    >
+      {images.map((image) => {
+        return (
+          <SwiperSlide key={image} onClick={() => handleBannerClick(image)}>
+            <img src={image} alt="" />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
